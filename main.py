8b0189c68f944751b66007315a5c40a3e258bd4f
dileepdominic <dileepdominic@gmail.com>
2020-01-01 12:00:28 +0530

    Updated code

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 
 
-print("Main file")
+def initializeHash():
+    print("initializeHash")
+
+def insertAppDetails(ApplicationRecords, name, phone, memRef, status):
+    print("insertAppDetails")
+
+def updateAppDetails(ApplicationRecords, name, phone, memRef, status):
+    print("updateAppDetails")
+
+def memRef(ApplicationRecords, memID):
+    print("memRef")
+
+def appStatus(StudentHashRecords):
+    print("appStatus")
+
+def destroyHash(StudentHashRecords):
+    print("destroyHash")
+
+
+def main():
+    print("Main file")
